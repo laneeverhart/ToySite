130108c55763bfa2fcf8e085548e1de88ca7ae84
laneeverhart <laneeverhart@yahoo.com>
2018-05-03 14:35:16 -0400

    Add files via upload

---
+++ ToySite2.js
@@ -0,0 +1,38 @@
+function zoomIn(event) {
+  var element = document.getElementById("overlay");
+  element.style.display = "inline-block";
+  var img = document.getElementById("imgZoom");
+  var posX = event.offsetX ? (event.offsetX) : event.pageX - img.offsetLeft;
+  var posY = event.offsetY ? (event.offsetY) : event.pageY - img.offsetTop;
+  element.style.backgroundPosition=(-posX*2)+"px "+(-posY*4)+"px";
+
+}
+
+function zoomOut() {
+  var element = document.getElementById("overlay");
+  element.style.display = "none";
+}
+
+
+function changeImage(event) {
+  event = event
+  var targetElement = event.target || event.srcElement;
+
+  if (targetElement.className == "thumb") {}
+  
+  document.getElementById("imgZoom").src = targetElement.getAttribute("src");
+  }
+
+  function changeToAnakinBox(){
+
+   document.getElementById("overlay").style.backgroundImage = "url(AnakinBox.jpg)";
+  } 
+
+  function changeToDarthVader(){
+
+   document.getElementById("overlay").style.backgroundImage = "url(DarthVader.jpg)";
+  } 
+  function changeToAnakinSet(){
+
+   document.getElementById("overlay").style.backgroundImage = "url(AnakinSet.jpg)";
+  } 
